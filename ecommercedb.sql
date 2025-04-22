bed4edea358089deeb6ab8dcff117cffca4e6242
migs2014 <migeledouglas@gmail.com>
2025-04-23 00:15:40 +0300

    database Tables code

--- ecommercedb.sql
+++ ecommercedb.sql
@@ -14,3 +14,21 @@ CREATE TABLE product_category (
     categoryName VARCHAR(255) NOT NULL,
     description TEXT
 );
+
+-- create product table
+CREATE TABLE product (
+    productID INT AUTO_INCREMENT PRIMARY KEY,
+    productName VARCHAR(255) NOT NULL,
+    brandID INT,
+    categoryID INT,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (brandID) REFERENCES brand(brandID),
+    FOREIGN KEY (categoryID) REFERENCES product_category(categoryID)
+);
+-- create product_ variation table
+CREATE TABLE product_variation (
+    variationID INT AUTO_INCREMENT PRIMARY KEY,
+    productID INT,
+    variationName  VARCHAR(50),
+    FOREIGN KEY (productID) REFERENCES product(productID)
+);
